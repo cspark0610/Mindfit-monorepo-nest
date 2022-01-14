a332c9de4e6d30fd8a16f32507c2381e5a2344ca
pablom58 <pablomvillamizar@gmail.com>
2022-01-14 10:57:32 -0400

    [FEATURE][278811] Create full basic test

--- src/evaluationTests/resolvers/satBasic.resolver.ts
+++ src/evaluationTests/resolvers/satBasic.resolver.ts
@@ -7,10 +7,6 @@ import { Repository } from 'typeorm';
 import { SatBasicDto } from '../dto/satBasic.dto';
 import { SatBasic } from '../models/satBasic.model';
 import { SatBasicService } from '../services/satBasic.service';
-import { SatBasicQuestionsService } from '../services/satBasicQuestion.service';
-import { SatBasicAnswersService } from '../services/satBasicQuestion.service copy';
-import { SatBasicSectionsService } from '../services/satBasicSection.service';
-
 @Resolver(() => SatBasic)
 @UseGuards(JwtAuthGuard)
 export class SatBasicsResolver extends BaseResolver(SatBasic, {
@@ -19,9 +15,6 @@ export class SatBasicsResolver extends BaseResolver(SatBasic, {
 }) {
   constructor(
     protected readonly service: SatBasicService,
-    private satSectionService: SatBasicSectionsService,
-    private satQuestionService: SatBasicQuestionsService,
-    private satAnswerService: SatBasicAnswersService,
 
     @InjectRepository(SatBasic)
     protected readonly repository: Repository<SatBasic>,
@@ -34,14 +27,7 @@ export class SatBasicsResolver extends BaseResolver(SatBasic, {
     @Args('data', { type: () => SatBasicDto })
     data: SatBasicDto,
   ): Promise<SatBasic> {
-    // console.log(data);
-    // console.log(data.satBasicSections);
-
-    const satTest = await this.service.create(data);
-
-    const result = await this.service.findOne(satTest.id, {
-      relations: ['sections'],
-    });
-    return result;
+    const satTest = await this.service.createFullTest(data);
+    return satTest;
   }
 }
--- src/evaluationTests/services/satBasic.service.ts
+++ src/evaluationTests/services/satBasic.service.ts
@@ -4,35 +4,55 @@ import { BaseService } from 'src/common/service/base.service';
 import { Repository } from 'typeorm';
 import { SatBasicDto } from '../dto/satBasic.dto';
 import { SatBasic } from '../models/satBasic.model';
+import { SatBasicQuestionsService } from './satBasicQuestion.service';
+import { SatBasicAnswersService } from './satBasicQuestion.service copy';
+import { SatBasicSectionsService } from './satBasicSection.service';
 
 @Injectable()
 export class SatBasicService extends BaseService<SatBasic> {
   constructor(
     @InjectRepository(SatBasic)
     protected readonly repository: Repository<SatBasic>,
-    private satBasicSectionService: SatBasicSectionService,
+    private satBasicSectionService: SatBasicSectionsService,
+    private satBasicQuestionService: SatBasicQuestionsService,
+    private satBasicAnswerService: SatBasicAnswersService,
   ) {
     super();
   }
 
-  asynccreateFullTest(data: SatBasicDto): Promise<SatBasic> {
-    const satTest = this.repository.create(data);
+  async createFullTest(data: SatBasicDto): Promise<SatBasic> {
+    const satTest = await this.create(data);
 
-    data.satBasicSections.forEach(async ({ title, questions }) => {
-      const section = await this.satSectionService.create({ satTest, title });
-
-      questions.forEach(async ({ satBasicAnswers: answers, ...data }) => {
-        const question = await this.satQuestionService.create({
-          section,
-          ...data,
+    // TODO Refactor
+    await Promise.all(
+      data.satBasicSections.map(async (section) => {
+        const sectionEntity = await this.satBasicSectionService.create({
+          satTest,
+          title: section.title,
         });
 
-        answers.forEach(async (answer) => {
-          return await this.satAnswerService.create({ question, ...answer });
-        });
-      });
-    });
+        await Promise.all(
+          section.questions.map(
+            async ({ satBasicAnswers: answers, ...data }) => {
+              const questionEntity = await this.satBasicQuestionService.create({
+                section: sectionEntity,
+                ...data,
+              });
 
-    return null as any;
+              await Promise.all(
+                answers.map(async (answer) =>
+                  this.satBasicAnswerService.create({
+                    question: questionEntity,
+                    ...answer,
+                  }),
+                ),
+              );
+            },
+          ),
+        );
+      }),
+    );
+
+    return this.findOne(satTest.id, { relations: ['sections'] });
   }
 }
