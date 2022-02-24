b9d0ad6df2a1ebbc789f98d63046c1b5b45e9bf8
cspark0610 <cpark0610@gmail.com>
2022-02-24 10:16:15 -0300

    feature mutation create coachNote

--- src/coaching/repositories/coachNote.repository.ts
+++ src/coaching/repositories/coachNote.repository.ts
@@ -1,6 +1,8 @@
 import { EntityRepository, SelectQueryBuilder } from 'typeorm';
 import { BaseRepository } from 'src/common/repositories/base.repository';
 import { CoachNote } from 'src/coaching/models/coachNote.model';
+import { Coach } from 'src/coaching/models/coach.model';
+import { Coachee } from 'src/coaching/models/coachee.model';
 
 @EntityRepository(CoachNote)
 export class CoachNoteRepository extends BaseRepository<CoachNote> {
@@ -9,5 +11,28 @@ export class CoachNoteRepository extends BaseRepository<CoachNote> {
       .createQueryBuilder('coachNote')
       .leftJoinAndSelect('coachNote.coach', 'coach')
       .leftJoinAndSelect('coachNote.coachee', 'coachee');
+  }
+
+  relationCoachNoteWithCoach(
+    coachNote: CoachNote,
+    coach: Coach,
+  ): Promise<void> {
+    return this.repository
+      .createQueryBuilder()
+      .relation(CoachNote, 'coach')
+      .of(coachNote)
+      .set(coach);
+    // as its a many-to-one relation
+  }
+
+  relationCoachNoteWithCoachee(
+    coachNote: CoachNote,
+    coachee: Coachee,
+  ): Promise<void> {
+    return this.repository
+      .createQueryBuilder()
+      .relation(CoachNote, 'coach')
+      .of(coachNote)
+      .set(coachee);
   }
 }
--- src/coaching/services/coachNote.service.ts
+++ src/coaching/services/coachNote.service.ts
@@ -2,10 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { CoachNote } from 'src/coaching/models/coachNote.model';
 import { CoachNoteRepository } from 'src/coaching/repositories/coachNote.repository';
 import { BaseService } from 'src/common/service/base.service';
+import { Coachee } from 'src/coaching/models/coachee.model';
+import { Coach } from 'src/coaching/models/coach.model';
+import { CoachNoteDto } from 'src/coaching/dto/coachNote.dto';
 
 @Injectable()
 export class CoachNoteService extends BaseService<CoachNote> {
   constructor(protected repository: CoachNoteRepository) {
     super();
+  }
+
+  async createCoachNoteWithCoachAndCocheeRelation(
+    data: Partial<CoachNoteDto>,
+    coach: Coach,
+    coachee: Coachee,
+  ): Promise<CoachNote> {
+    const coachNote = await this.repository.create(data);
+    if (coachNote && coach && coachee) {
+      await Promise.all([
+        this.repository.relationCoachNoteWithCoach(coachNote, coach),
+        this.repository.relationCoachNoteWithCoachee(coachNote, coachee),
+      ]);
+      return coachNote;
+    }
   }
 }
