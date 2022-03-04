c0b774bba2b28f70549579b868fa8b2cb941754e
David Abreu <biorazor.a@gmail.com>
2022-03-04 13:47:50 -0400

    [FEATURE][60bf8a] Implement Coachee Evaluation Resolver
    Update Service, repository and DTO's

--- src/coaching/dto/coacheeEvaluation.dto.ts
+++ src/coaching/dto/coacheeEvaluation.dto.ts
@@ -1,31 +1,36 @@
-import { IsNotEmpty, IsPositive, IsString } from 'class-validator';
-import { Coach } from 'src/coaching/models/coach.model';
+import { Field, InputType, OmitType } from '@nestjs/graphql';
+import { IsNotEmpty, IsPositive, IsString, MaxLength } from 'class-validator';
 import { Coachee } from 'src/coaching/models/coachee.model';
 import { CoacheeEvaluation } from 'src/coaching/models/coacheeEvaluation.model';
 import { getEntity } from 'src/common/functions/getEntity';
 
-export class CoacheeEvaluationDto {
-  @IsPositive()
-  @IsNotEmpty()
-  coachId: number;
-
+@InputType()
+export class CreateCoacheeEvaluationDto {
+  @Field()
   @IsPositive()
   @IsNotEmpty()
   coacheeId: number;
 
+  @Field()
   @IsString()
   @IsNotEmpty()
+  @MaxLength(1000)
   evaluation: string;
 
   public static async from(
-    dto: CoacheeEvaluationDto,
+    dto: CreateCoacheeEvaluationDto,
   ): Promise<Partial<CoacheeEvaluation>> {
-    const { coachId, coacheeId, ...coacheeEvaluationData } = dto;
+    const { coacheeId, ...coacheeEvaluationData } = dto;
 
     return {
       ...coacheeEvaluationData,
-      coach: coachId ? await getEntity(coachId, Coach) : null,
       coachee: coacheeId ? await getEntity(coacheeId, Coachee) : null,
     };
   }
 }
+
+@InputType()
+export class UpdateCoacheeEvaluationDto extends OmitType(
+  CreateCoacheeEvaluationDto,
+  ['coacheeId'],
+) {}
--- src/coaching/services/coacheeEvaluation.service.ts
+++ src/coaching/services/coacheeEvaluation.service.ts
@@ -1,11 +1,127 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import {
+  CreateCoacheeEvaluationDto,
+  UpdateCoacheeEvaluationDto,
+} from 'src/coaching/dto/coacheeEvaluation.dto';
+import { CoachingErrorEnum } from 'src/coaching/enums/coachingErrors.enum';
 import { CoacheeEvaluation } from 'src/coaching/models/coacheeEvaluation.model';
 import { CoacheeEvaluationRepository } from 'src/coaching/repositories/coacheeEvaluation.repository';
+import { CoacheeService } from 'src/coaching/services/coachee.service';
+import { MindfitException } from 'src/common/exceptions/mindfitException';
 import { BaseService } from 'src/common/service/base.service';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
 export class CoacheeEvaluationService extends BaseService<CoacheeEvaluation> {
-  constructor(protected repository: CoacheeEvaluationRepository) {
+  constructor(
+    protected repository: CoacheeEvaluationRepository,
+    private userService: UsersService,
+    private coacheeService: CoacheeService,
+  ) {
     super();
+  }
+
+  async coachCreateCoacheeEvaluation(
+    userId: number,
+    data: CreateCoacheeEvaluationDto,
+  ): Promise<CoacheeEvaluation> {
+    const user = await this.userService.findOne(userId);
+
+    if (!user.coach) {
+      throw new MindfitException({
+        error: 'You do not have a Coach profile',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.NO_COACH_PROFILE,
+      });
+    }
+
+    // get from repository do not return relations
+    const coachee = await this.coacheeService.findOne(data.coacheeId);
+
+    if (coachee?.assignedCoach?.id != user.coach.id) {
+      throw new MindfitException({
+        error: 'The coachee is not assigned to you.',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.COACHEE_NOT_ASSIGNED_TO_COACH,
+      });
+    }
+
+    //Exclude coacheeId from data, and queried coachee is used
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { coacheeId, ...rest } = data;
+
+    return this.repository.create({
+      coach: user.coach,
+      coachee,
+      ...rest,
+    });
+  }
+
+  async coachUpdateCoacheeEvaluation(
+    userId: number,
+    evaluationId: number,
+    data: UpdateCoacheeEvaluationDto,
+  ): Promise<CoacheeEvaluation> {
+    const user = await this.userService.findOne(userId);
+
+    if (!user.coach) {
+      throw new MindfitException({
+        error: 'You do not have a Coach profile',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.NO_COACH_PROFILE,
+      });
+    }
+
+    const coacheeEvaluation = await this.findOne(evaluationId);
+
+    if (coacheeEvaluation.coach.id != user.coach.id) {
+      throw new MindfitException({
+        error: 'The Coachee Evaluation does not belong to you.',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.NOT_COACHEE_EVALUATION_OWNER,
+      });
+    }
+
+    if (coacheeEvaluation.coachee?.assignedCoach?.id != user.coach.id) {
+      throw new MindfitException({
+        error: 'The coachee is no longer assigned to you.',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.COACHEE_NOT_ASSIGNED_TO_COACH,
+      });
+    }
+
+    return this.update(evaluationId, data);
+  }
+
+  async coachDeleteCoacheeEvaluation(userId: number, evaluationId: number) {
+    const user = await this.userService.findOne(userId);
+
+    if (!user.coach) {
+      throw new MindfitException({
+        error: 'You do not have a Coach profile',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.NO_COACH_PROFILE,
+      });
+    }
+
+    const coacheeEvaluation = await this.findOne(evaluationId);
+
+    if (coacheeEvaluation.coach.id != user.coach.id) {
+      throw new MindfitException({
+        error: 'The Coachee Evaluation does not belong to you.',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.NOT_COACHEE_EVALUATION_OWNER,
+      });
+    }
+
+    if (coacheeEvaluation.coachee?.assignedCoach?.id != user.coach.id) {
+      throw new MindfitException({
+        error: 'The coachee is no longer assigned to you.',
+        statusCode: HttpStatus.FORBIDDEN,
+        errorCode: CoachingErrorEnum.COACHEE_NOT_ASSIGNED_TO_COACH,
+      });
+    }
+
+    return this.delete(evaluationId);
   }
 }
