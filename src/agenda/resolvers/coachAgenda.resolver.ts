936ac078bb8fb3181a2fd0f5d37b2d586f306073
David Abreu <biorazor.a@gmail.com>
2022-03-23 16:22:13 -0400

    remove console log

--- src/agenda/resolvers/coachAgenda.resolver.ts
+++ src/agenda/resolvers/coachAgenda.resolver.ts
@@ -81,8 +81,6 @@ export class CoachAgendaResolver extends BaseResolver(CoachAgenda, {
 
     await this.coacheeService.validateCoacheeHaveSelectedCoach(coachee);
 
-    console.log(coachee);
-
     const coachAgenda = coachee.assignedCoach.coachAgenda;
 
     if (coachAgenda.outOfService) {
